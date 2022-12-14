79cc397bb0a578f885bb78d897299e5c21071770
Adrian <afernandeztorres@danielcastelao.org>
2022-12-14 14:23:59 +0100

    adri

--- LiderColaborador/src/Main.java
+++ LiderColaborador/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Nashe" + "\nDamian GOZ\nPrueba lider ");
-
+        System.out.println("Yorch le da a las casas");
 
     }
 }
