f07fa13871deaae7e0dea268a29c8a754cb6974f
JorgeUsatorre <usatorre1210@gmail.com>
2022-12-14 13:58:30 +0100

    Commit Jorge

--- LiderColaborador/src/Main.java
+++ LiderColaborador/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Nashe");
+        System.out.println("Nashe" + "\nDamian GOZ\nPrueba lider ");
+
+
     }
 }
